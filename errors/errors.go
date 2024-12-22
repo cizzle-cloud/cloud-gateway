0fb0f0674b1af39a03e2f91694c11abf14bc8fbe
root <root@DESKTOP-FNVRE4H.>
2024-12-22 19:13:44 +0100

    Add: project

---
+++ errors/errors.go
@@ -0,0 +1,39 @@
+package errors
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+type ErrorHandler interface {
+	error
+	Handle()
+}
+
+type ContextError struct {
+	Code    int
+	Message string
+	Context *gin.Context
+}
+
+func (e *ContextError) Error() string {
+	return e.Message
+}
+
+func (e *ContextError) Handle() {
+	e.Context.JSON(e.Code, gin.H{"error": e.Message})
+	e.Context.AbortWithStatus(e.Code)
+}
+
+type LoadConfigError struct {
+	Message string
+}
+
+func (e *LoadConfigError) Error() string {
+	return e.Message
+}
+
+func (e *LoadConfigError) Handle() {
+	log.Fatalf("Could not load config: %s", e.Message)
+}
